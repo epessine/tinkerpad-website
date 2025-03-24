ed15de3bac6e83b857ae6b6d543cebc47bf9a87c
Eduardo Pessine <eduardopessine@gmail.com>
2025-03-24 18:50:10 -0300

    init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css';
+import Alpine from 'alpinejs';
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
+
+window.Alpine = Alpine;
+window.tippy = tippy;
+
+Alpine.start();
